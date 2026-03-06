d86441b40bbd860ade3fad3de0fcc706267e3fc3
jesus <jesusleandro16@gmail.com>
2026-03-06 18:28:33 -0400

    implementacion-componentes-faltantes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import gsap from 'gsap';
-import { ScrollTrigger, SplitText} from "gsap/all";
-import Navbar from "./components/Navbar.jsx";
-import Hero from "./components/Hero.jsx";
+import { ScrollTrigger, SplitText } from "gsap/all";
 
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import Cocktails from './components/Cocktails.jsx'
+import About from './components/About.jsx'
+import Art from './components/Art.jsx'
+import Menu from './components/Menu.jsx'
+import Contact from './components/Contact.jsx'
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -10,9 +15,14 @@ const App = () => {
     return (
         <main>
             <Navbar />
-            <Hero/>
-
+            <Hero />
+            <Cocktails />
+            <About />
+            <Art />
+            <Menu />
+            <Contact />
         </main>
     )
 }
+
 export default App
